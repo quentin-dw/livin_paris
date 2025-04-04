a4525477d580db9eb84cf63933f525f4fc7a47ea
tomdcx <tdehecohen@gmail.com>
2025-04-04 14:21:30 +0200

    ajout projet test

---
+++ Livin_paris-WinFormsApp/TestProject1/Test1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace TestProject1
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod_DML_SQL()
+        {
+            string input = "INSERT INTO Ingrédient (nom_ingredient, type) VALUES ('chocolat', 'dessert');";
+            bool result = Program.DML_SQL(input);
+        }
+    }
+}
