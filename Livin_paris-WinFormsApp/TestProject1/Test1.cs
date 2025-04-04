4f6a8165549caa89c5f21df0e73e2b0c197eabfd
tomdcx <tdehecohen@gmail.com>
2025-04-04 14:36:49 +0200

    maj projet test

--- Livin_paris-WinFormsApp/TestProject1/Test1.cs
+++ Livin_paris-WinFormsApp/TestProject1/Test1.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
+﻿using Livin_paris_WinFormsApp;
 
 namespace TestProject1
 {
@@ -6,10 +6,19 @@ namespace TestProject1
     public sealed class Test1
     {
         [TestMethod]
-        public void TestMethod_DML_SQL()
+        public void TestMethod_DML_SQL_True()
         {
             string input = "INSERT INTO Ingrédient (nom_ingredient, type) VALUES ('chocolat', 'dessert');";
             bool result = Program.DML_SQL(input);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TestMethod_DML_SQL_False()
+        {
+            string input = "INSERT INTO Ingrédient (a, b) VALUES ('chocolat', 'dessert');";
+            bool result = Program.DML_SQL(input);
+            Assert.IsFalse(result);
         }
     }
 }
